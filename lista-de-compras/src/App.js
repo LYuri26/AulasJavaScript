597595da7715db4ef0690f45befbbf1ac5fa1266
Lenon Yuri <lenonyurisilva26@gmail.com>
2023-10-15 16:52:28 -0300

    15/10/23

---
+++ lista-de-compras/src/App.js
@@ -0,0 +1,11 @@
+import ListaDeCompras from './components/ListaDeCompras';
+
+function App() {
+  return (
+    <div className="App">
+      <ListaDeCompras />
+    </div>
+  );
+}
+
+export default App;
