6427f0aae4a33f048b3fe41ba09aeb1823aab37f
Lenon Yuri <lenonyurisilva26@gmail.com>
2023-10-15 20:22:42 -0300

    15/10/23

---
+++ ficha-rpg/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import FichaRPG from './FichaRPG';
+
+function App() {
+  return (
+    <div className="App">
+      <FichaRPG />
+    </div>
+  );
+}
+
+export default App;
---
+++ ficha-rpg/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Certifique-se de ter o arquivo index.css na mesma pasta.
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
