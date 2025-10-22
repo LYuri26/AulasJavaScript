acf70a27ac638b1041a2e8cac8737e13eda29ca5
Lenon Yuri <116133424+LYuri26@users.noreply.github.com>
2025-10-22 14:10:46 -0300

    Delete Exercicios TF directory

--- Exercicios TF/Atividade 2/passoapassocalculadora/display/script.js
+++
@@ -1,5 +0,0 @@
-function mostrarNumero(numero) {
-  // Mostra o número clicado no display
-  const display = document.getElementById("display");
-  display.textContent = `Você clicou no número: ${numero}`;
-}
